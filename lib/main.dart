78c2910e574c6801efe8e8aa4509643a791a0083
Yolisses <iamurise@gmail.com>
2025-08-05 08:39:49 -0300

    Fix build

--- lib/main.dart
+++ lib/main.dart
@@ -97,13 +97,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text('You have pushed the button this many times:'),
+            Text('$_counter'),
           ],
         ),
       ),
